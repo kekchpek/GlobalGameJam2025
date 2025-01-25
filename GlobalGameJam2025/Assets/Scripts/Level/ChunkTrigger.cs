167861512ebaa0f89c20ad5ca2746544c9f89e3d
EdvinasLep <edvinas.lep@gmail.com>
2025-01-25 11:03:28 +0300

    Add chunk generator

---
+++ GlobalGameJam2025/Assets/Scripts/Level/ChunkTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkTrigger : MonoBehaviour
+{
+    MapController mapController;
+    public GameObject targetMap;
+
+    void Awake()
+    {
+        mapController = FindFirstObjectByType<MapController>();
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            mapController.currentChunk = targetMap;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            mapController.currentChunk = null;
+        }
+    }
+}
---
+++ GlobalGameJam2025/Assets/Scripts/Level/MapController.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TerrainUtils;
+
+
+public class MapController : MonoBehaviour
+{
+    public GameObject LevelChunk;
+    public Transform chunkParent;
+    public Transform player;
+    public GameObject currentChunk;
+
+    public float checkerRadius = 2f;
+    public float optimizerCooldownDur;
+    public float maxOptimizeDistance;
+    public LayerMask levelMask;
+
+    private float optimizerCooldown;
+
+    private List<GameObject> spawnedChunks = new List<GameObject>();
+
+    private void Start()
+    {
+        Instantiate(LevelChunk, Vector3.zero, Quaternion.identity, chunkParent);
+    }
+    void Update()
+    {
+        CheckAndSpawnChunks();
+        ChunkOptimizer();
+    }
+
+    void CheckAndSpawnChunks()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(player.position, checkerRadius, levelMask);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject == currentChunk)
+            {
+                
+                SpawnChunk();
+                
+                break;
+            }
+        }
+    }
+
+    void SpawnChunk()
+    {
+        Vector2 nextChunk = currentChunk.transform.Find("NextChunk").position;
+
+        if (!Physics2D.OverlapPoint(nextChunk, levelMask))
+        {
+            GameObject newChunk1 = Instantiate(LevelChunk, nextChunk, Quaternion.identity, chunkParent);
+            spawnedChunks.Add(newChunk1);
+        }
+    }
+
+    private void ChunkOptimizer()
+    {
+        optimizerCooldown -= Time.deltaTime;
+
+        if (optimizerCooldown <= 0f)
+        {
+            optimizerCooldown = optimizerCooldownDur;
+        }
+        else
+        {
+            return;
+        }
+
+        foreach (GameObject chunk in spawnedChunks)
+        {
+            float opDist = Vector3.Distance(player.transform.position, chunk.transform.position);
+            if (opDist > maxOptimizeDistance)
+            {
+                chunk.SetActive(false);
+            }
+            else
+            {
+                chunk.SetActive(true);
+            }
+        }
+    }
+}
+
+
+
+
+
