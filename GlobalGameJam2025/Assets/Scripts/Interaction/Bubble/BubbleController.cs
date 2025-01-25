be2e2796863c003a40fd260ca437cd1bb237358c
EdvinasLep <edvinas.lep@gmail.com>
2025-01-25 17:17:48 +0300

    Display bubble hp

--- GlobalGameJam2025/Assets/Scripts/Interaction/Bubble/BubbleController.cs
+++ GlobalGameJam2025/Assets/Scripts/Interaction/Bubble/BubbleController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 namespace BubbleJump
 {
@@ -6,15 +7,25 @@ namespace BubbleJump
     {
         [SerializeField]
         private int _hp;
-        private Animator _animator;
 
-        void Start()
+        private Animator _animator;
+        private TextMeshProUGUI _hpText;
+        private float _velocityTreshold;
+
+        private void Start()
         {
+            _hpText = GetComponentInChildren<TextMeshProUGUI>();
             _hp = Random.Range(3, 7);
+            UpdateHp();
             //_animator = GetComponent<Animator>();
         }
 
-        void Update()
+        private void UpdateHp()
+        {
+            _hpText.text = _hp.ToString();
+        }
+
+        private void Update()
         {
             if(_hp <= 0)
             {
@@ -25,12 +36,26 @@ namespace BubbleJump
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            
-            if (collision.collider.CompareTag("Player"))
+            if (collision.collider.CompareTag("Player")) ;
             {
-                //_hp--;
-                //_animator.SetTrigger("Shake");
+                Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+                Vector2 orgVelocity = rb.linearVelocity;
+
+                Debug.Log($"Player Velocity Magnitude: {orgVelocity.sqrMagnitude}");
+
+                if (orgVelocity.sqrMagnitude >= _velocityTreshold)
+                {
+                    Physics2D.IgnoreCollision(GetComponent<CircleCollider2D>(), collision.collider, true);
+                    rb.linearVelocity = orgVelocity;
+                    _hp = 0;
+                }
+                else
+                {
+                    _hp--;
+                    UpdateHp();
+                }
             }
+            
         }
     }
 }
