2f35f2e445bd7c47bbaf78912dce3966ce47ae4b
Nikolai Selivanov <nikolaiselivanov@Nikolais-MacBook-Pro.local>
2025-01-25 18:30:29 +0200

    Change camera controller

--- GlobalGameJam2025/Assets/Scripts/Interaction/CameraController.cs
+++ GlobalGameJam2025/Assets/Scripts/Interaction/CameraController.cs
@@ -9,15 +9,12 @@ namespace BubbleJump
         private Transform _player;
         [SerializeField]
         private float zOffset;
-
-        private void Start()
-        {
-            //player = FindFirstObjectByType<Player>;
-        }
+        [SerializeField]
+        private float yOffset;
 
         private void Update()
         {
-            transform.position = new Vector3(transform.position.x, _player.transform.position.y, zOffset);
+            transform.position = new Vector3(transform.position.x, _player.transform.position.y + yOffset, zOffset);
         }
     }
 }
